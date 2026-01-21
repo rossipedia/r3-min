0958d20034a0e66b0f41654813ebf5e9e65e1a9d
rossipedia <bryan.ross@docker.com>
2026-01-20 17:21:02 -0700

    update with R3 Alpha

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,23 @@
 import { createRoot, type Handle } from "@remix-run/component";
 
-function App(this: Handle) {
-  return (
+function App(handle: Handle) {
+  let count = 0;
+
+  return () => (
     <>
       <h1 class="text-3xl font-black">Hello Remix 3</h1>
       <p>Scratch pad</p>
+      <button
+        class="border border-solid border-gray-400 rounded bg-gray-200 active:translate-y-px px-2 py-1 shadow-sm active:shadow-none"
+        on={{
+          click: () => {
+            count++;
+            handle.update();
+          },
+        }}
+      >
+        {count}
+      </button>
     </>
   );
 }
